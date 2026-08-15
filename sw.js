acc04b92e5f02c95bf195f43c9d3a03733de2b03
andy0958080291-png <andy0958080291@gmail.com>
2026-08-16 01:36:02 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='squat40-v4';
+const CACHE='squat40-v5';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',e=>{
